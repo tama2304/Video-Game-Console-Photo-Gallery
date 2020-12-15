87b115f613d27d51d8a224548c72ae07ce2b1859
tama2304 <43211704+tama2304@users.noreply.github.com>
2020-12-15 10:25:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$("document").ready(function() {
 var photos = ["https://upload.wikimedia.org/wikipedia/commons/7/76/Nintendo-Switch-Console-Docked-wJoyConRB.jpg", "https://cdn.mos.cms.futurecdn.net/7Qv4q73m9Nif9CpxtfVWs6.jpg", "https://www.nme.com/wp-content/uploads/2020/07/Xbox-Series-X.jpg"];
 
 var consoles = ["Nintendo Switch", "PlayStation 5", "Xbox Series X"];
@@ -17,3 +18,4 @@ $(".gallery").append("<img src =" + photos[photo] + ">" + "<p>" + consoles[photo
 });
     //$(".gallery").append("<img src=" + inputValue + ">" + "<p>" + consoleValue + "</p>");
 	});
+});
